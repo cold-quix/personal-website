fc465fab33f27244f214c04d2dfa1f458aa87e13
Jack Park <Jlazarskiparkin9815@conestogac.on.ca>
2021-06-28 12:35:35 -0400

    Fixed typos in media.html  Added files for pokertracker.

---
+++ js/pokertracker.js
@@ -0,0 +1,10 @@
+/*
+	FILE			: pokertracker.js
+    PROJECT			: Jack Parkinson's personal website
+    PROGRAMMER		: Jack Parkinson
+    FIRST VERSION	: 2021-06-25
+    DESCRIPTION		:
+        Javascript for the pokertracker page.
+*/
+
+

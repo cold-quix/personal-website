69c57097d46a6f60ac12c8220db9d367bdab1701
Jack Park <Jlazarskiparkin9815@conestogac.on.ca>
2021-07-01 22:54:03 -0400

    Progress on pokertracker HTML version.

--- js/pokertracker.js
+++ js/pokertracker.js
@@ -7,4 +7,66 @@
         Javascript for the pokertracker page.
 */
 
+// Variables
+var BBMFState = "BB";
+
+// Functions to run on page load
+window.addEventListener("load", setListeners());
+
+function test() {
+	alert("test successful");
+}
+
+/*
+	NAME		: toggleBBMF
+	PARAMETERS	: None
+	RETURN		: None
+	DESCRIPTION	: 
+		Toggles the big blind/M-factor label and display depending on its context.
+*/
+function toggleBBMF() {
+	if (BBMFState == "BB") {
+		//set to mf
+		document.getElementById("ID-bbmf_label").innerHTML = "MF";
+		BBMFState = "MF";
+	}
+	else {
+		// set to bb
+		document.getElementById("ID-bbmf_label").innerHTML = "BB";
+		BBMFState = "BB";
+	}
+}
+
+/*
+	NAME		: 
+	PARAMETERS	: 
+	RETURN		: 
+	DESCRIPTION	: 
+		
+*/
+
+
+/*
+	NAME		: 
+	PARAMETERS	: 
+	RETURN		: 
+	DESCRIPTION	: 
+		
+*/
+
+
+/*
+	NAME		: setListeners
+	PARAMETERS	: None
+	RETURN		: None
+	DESCRIPTION	: 
+		Function which attaches event listeners to page elements. Called 
+		on page load.
+*/
+function setListeners() {
+	var listenElement = document.getElementById("ID-bbmf_toggle");
+	listenElement.addEventListener("click", function(){toggleBBMF()}, false);
+}
+
+
 
